36e35bf5dc021fd1e1c563ce8559140630af1bbf
Myphz <danielscanu45@outlook.it>
2022-06-25 10:42:18 +0200

    feature: initialize table game style

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,7 @@
 <script setup>
 import Game from './components/Game.vue';
 </script>
+
+<style lang="sass">
+  @use "/src/assets/sass/utilities" as *
+</style>
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>
-    test
-  </div>
+  <main class="justify-center align-center">
+    <table>
+      <tr v-for="i in 11">
+        <td v-for="i in 13">GRID</td>
+      </tr>
+    </table>
+  </main>
 </template>
 
 <script setup>
 </script>
 
 <style lang="sass" scoped>
+  main
+    width: 100vw
+    height: 100vh
+    background-color: yellow
+
+    table
+      border-collapse: collapse
+      width: 1300px
+      height: 1100px
+
+      td
+        text-align: center
+        background-color: green
+        border: 2px solid blue
 </style>
