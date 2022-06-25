8990017b0e58cf2243b8eaf12e0e656aa237ae86
Myphz <danielscanu45@outlook.it>
2022-06-25 22:55:13 +0200

    feature: add bomb explode animation

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -8,7 +8,9 @@
       </table>
 
       <div class="player" :style="`top: ${top}px; left: ${left}px`"></div>
-      <div v-for="[x, y] in bombs" :key="`${x}-${y}`" class="bomb" :style="`top: ${x+25}px; left: ${y+25}px`"></div>
+      <div>
+        <div v-for="[x, y] in bombs" :key="`${x}-${y}`" class="bomb" :style="`top: ${x*100+25}px; left: ${y*100+25}px; --power: ${power}`"></div>
+      </div>
     </section>
   </main>
 </template>
@@ -30,6 +32,7 @@ const classes = [...Array(ROWS).keys()].map(i => [
 
 const top = ref(0);
 const left = ref(0);
+const power = ref(1);
 
 let bombs_available = 1;
 const bombs = reactive([]);
@@ -91,8 +94,14 @@ function move(direction, multiplicator) {
 function placeBomb() {
   if (bombs.length === bombs_available) return;
   const [cellX, cellY] = getCell(top.value, left.value);
-  bombs.push([cellX*100, cellY*100]);
+  bombs.push([cellX, cellY]);
+  setTimeout(animateBomb, BOMB_TIME - 1000);
   setTimeout(clearBomb, BOMB_TIME);
+};
+
+function animateBomb() {
+  const bomb = document.getElementsByClassName("bomb")[0];
+  bomb.classList.add("bomb-explosion");
 };
 
 function clearBomb() {
@@ -153,6 +162,25 @@ setInterval(loop, UPDATE_TIME);
     height: 50px
     width: 50px
     background-color: yellow
+
+  .bomb-explosion
+    &::before, &::after
+      content: ""
+      position: absolute
+      border-radius: 50px
+      background-color: orange
+
+    &::before
+      left: 0
+      animation: explosion-top 1s linear 2s forwards
+      top: calc(-100px * var(--power))
+      height: calc(200px * var(--power) + 50px)
+      width: 50px
+
+    &::after
+      left: calc(-100px * var(--power))
+      height: 50px
+      width: calc(200px * var(--power) + 50px)
 
   .free
     background-color: white
